5c2184fcc70eee1601271364464af09a2808f28e
Djibrildaniels <ed.mensah89@gmail.com>
2024-07-30 17:27:58 +0200

    30thJuly update

---
+++ TrueBalances/Models/Expense.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TrueBalances.Models;
+
+public class Expense
+{
+    public int Id { get; set; }
+    [Required]
+    public string Title { get; set; }
+    [Required]
+    public decimal Amount { get; set; }
+    public DateTime Date { get; set; }
+    public string Category { get; set; }
+    public string UserId { get; set; }
+}
