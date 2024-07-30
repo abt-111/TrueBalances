664af72cb8fde23c9c3949f41d4f29976cba8b4e
abt-111 <anthony.temmel@gmail.com>
2024-07-30 15:46:29 +0200

    Ajout des propriété FistName et LastName dans la classe CustomUser

--- TrueBalances/Areas/Identity/Data/CustomUser.cs
+++ TrueBalances/Areas/Identity/Data/CustomUser.cs
@@ -9,5 +9,8 @@ namespace TrueBalances.Areas.Identity.Data;
 // Add profile data for application users by adding properties to the CustomUser class
 public class CustomUser : IdentityUser
 {
+    [PersonalData]
+    public string? FirstName { get; set; }
+    [PersonalData]
+    public string? LastName { get; set; }
 }
-
