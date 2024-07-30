910b4b55373031aefa52f078050c52eb7d68d020
Djibrildaniels <ed.mensah89@gmail.com>
2024-07-30 17:33:35 +0200

    ok

---
+++ TrueBalances/Controllers/ExpenseController.cs
@@ -0,0 +1,82 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrueBalances.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        // GET: ExpenseController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ExpenseController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ExpenseController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ExpenseController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ExpenseController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ExpenseController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ExpenseController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ExpenseController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
