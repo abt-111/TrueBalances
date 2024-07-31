caace025d01d349086235867c6cdd32dd5a665e4
abt-111 <anthony.temmel@gmail.com>
2024-07-31 16:32:56 +0200

    Ajout ébauche de différente version d'affichage selon le statut d'authetification

---
+++ TrueBalances/Models/UserViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TrueBalances.Models
+{
+    public class UserViewModel
+    {
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
