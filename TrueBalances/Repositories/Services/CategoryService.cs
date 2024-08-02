f6b9c8aa394b6d8db6b15d34204c351d4ca41058
khaou <khaou@JULIA>
2024-08-02 16:00:51 +0200

    Modification dans le dossier Service

--- TrueBalances/Repositories/Services/CategoryService.cs
+++ TrueBalances/Repositories/Services/CategoryService.cs
@@ -1,11 +1,54 @@
-﻿using TrueBalances.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using TrueBalances.Data;
+using TrueBalances.Models;
 using TrueBalances.Repositories.Interfaces;
 
 namespace TrueBalances.Repositories.Services
 {
-    //public class CategoryService : ICategory
-    //{
-        
+    public class CategoryService : ICategoryRepository
+    {
+        private readonly UserContext _context;
 
-    //}
+        public CategoryService(UserContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Category>> GetAllCategoriesAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category?> GetCategoryByIdAsync(int id)
+        {
+            return await _context.Categories.FindAsync(id);
+        }
+
+        public async Task AddCategoryAsync(Category category)
+        {
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateCategoryAsync(Category category)
+        {
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteCategoryAsync(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<bool> CategoryExistsAsync(int id)
+        {
+            return await _context.Categories.AnyAsync(e => e.Id == id);
+        }
+    }
 }
