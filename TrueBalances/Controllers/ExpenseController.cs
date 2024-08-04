70cdeb1c9602039b41c12f5b654a6890df4c1e08
abt-111 <anthony.temmel@gmail.com>
2024-08-04 17:49:36 +0200

    Ajout de l'attribut [Autorize] vérouillant les fonctionnalité aux utilisateurs non-connecté

--- TrueBalances/Controllers/ExpenseController.cs
+++ TrueBalances/Controllers/ExpenseController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -8,6 +9,7 @@ using TrueBalances.Models;
 
 namespace TrueBalances.Controllers
 {
+    [Authorize]
     public class ExpenseController : Controller
     {
         private readonly Data.DbContext _context;
